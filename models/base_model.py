2b85aa9d3b69a95039106f7906447841f04fcd9d
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-07-17 03:47:51 +0300

    chore: update the base model revert

--- models/base_model.py
+++ models/base_model.py
@@ -1,65 +1,102 @@
 #!/usr/bin/python3
+
 """
-A module that implements the BaseModel class
+Module for the BaseModel class.
+
+This module defines the BaseModel class, which is a
+base class that defines common attributes and methods for
+other classes.
+
 """
 
-from uuid import uuid4
+from models import storage
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
     """
-    A class that defines all common attributes/methods for other classes
+    A base class that defines common attributes and methods for other classes.
+
+    Public instance attributes:
+    - id: string - A unique identifier assigned using uuid when an instance is
+    created.
+    - created_at: datetime - The datetime when an instance is created.
+    - updated_at: datetime - The datetime when an instance is last updated.
+
+    Public instance methods:
+    - save(self): Updates the public instance attribute updated_at with the
+    current datetime and calls the save() method of storage.
+    - to_dict(self): Returns a dictionary representation of the BaseModel
+    instance.
+
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Initialize the BaseModel class
-        """
+        Initializes a new instance of the BaseModel class.
 
-        from models import storage
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
-        else:
+        If kwargs is not empty, it recreates an instance using the dictionary
+        representation. Otherwise, it creates a new instance with a new id
+        and created_at.
+
+        Args:
+            *args: Not used.
+            **kwargs: A dictionary representing the instance attributes.
+
+        """
+        if kwargs:
+            if 'id' not in kwargs:
+                kwargs['id'] = str(uuid.uuid4())
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             for key, value in kwargs.items():
                 if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """
-        Returns the string representation of BaseModel object.
-        [<class name>] (<self.id>) <self.__dict__>
+        Returns a string representation of the BaseModel instance.
+
+        The string includes the class name, id, and instance attributes.
+
+        Returns:
+            str: A string representation of the BaseModel instance.
+
         """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id,
-                                     self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        Updates 'self.updated_at' with the current datetime
+        Updates the public instance attribute updated_at with the current
+        datetime and calls the save() method of storage.
+
         """
-        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__
-        of the instance:
+        Returns a dictionary representation of the BaseModel instance.
 
-        - only instance attributes set will be returned
-        - a key __class__ is added with the class name of the object
-        - created_at and updated_at must be converted to string object in ISO
-        object
+        The dictionary contains all instance attributes, including the
+        class name, id, created_at, and updated_at attributes.
+
+        Returns:
+            dict: A dictionary representation of the BaseModel instance.
+
         """
-        dict_1 = self.__dict__.copy()
-        dict_1["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if k in ("created_at", "updated_at"):
-                v = self.__dict__[k].isoformat()
-                dict_1[k] = v
-        return dict_1
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
